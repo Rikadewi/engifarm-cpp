ee12df74db9a9e0b2c207318eb1ebde0e88d340b
Rika Dewi <rika_4444@yahoo.com>
2019-03-12 20:03:25 +0700

    Initialize player.h and livingthings.h

---
+++ livingthings.h
@@ -0,0 +1,12 @@
+// livingthings.h
+#ifndef __LIVINGTHINGS_H__
+#define __LIVINGTHINGS_H__
+
+class LivingThings {
+    public:
+  
+    private:
+
+};
+
+#endif 
---
+++ player.h
@@ -0,0 +1,18 @@
+// player.h
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+#include "livingthings.h"
+
+class Player: public LivingThings {
+    public:
+        int water;
+        List <Product> inventory;
+        long money;
+    private:
+        void talk();
+        void interact();
+        void kill();
+        void grow();
+};
+
+#endif 
