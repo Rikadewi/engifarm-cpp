06571b54f8d22db84aeaf1c8260e83508569ea07
gardahadi <garda4199@gmail.com>
2019-03-19 20:36:06 +0700

    Added list

---
+++ list.h
@@ -0,0 +1,19 @@
+template <class Type>
+class List {
+    public:
+        //ctor-cctor-dtor
+        List(); //default Ctor
+        List(int); // Ctor dengan parameter
+        List(const List<Type>&); // Cctor
+        ~List();
+        List& operator= (const List<Type>&);
+
+        //Services
+        bool isEmpty() const;
+        bool isFull() const;
+        void add(Type);
+        void remove(Type elmt);
+        int find(Type elmt);
+        Type get(int indeks);
+
+}
