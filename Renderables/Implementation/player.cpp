1d1e287ace4131eee8340d6dff2d82e1c084b45e
rika <rika_4444@yahoo.com>
2019-03-25 00:53:17 +0700

    Add move method

--- Renderables/Implementation/player.cpp
+++ Renderables/Implementation/player.cpp
@@ -5,9 +5,16 @@ Player::Player() : water(DEFWATER), money(DEFMONEY){
 
 }
 
-void Player::talk(){
+void Player::move(){
     List<char> around = lookAround();
-    if (around.find(charLand) != -1){
-
+    int idx = around.find(charLand);
+    if(idx == -1){
+        idx = around.find(charLandBerumput);
+    }
+    if(idx == -1){
+        throw "Tidak ada space";
+    }else{
+        setY(idx%COL);
+        setX(idx/COL);
     }
 }
